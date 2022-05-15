24a87ec49c63cdfd65c5a0f4bd23909b49cf893d
maryamosama33 <mazen2003khaled@gmail.com>
2022-05-15 09:59:15 +0200

    check password

--- test.cpp
+++ test.cpp
@@ -39,3 +39,81 @@ void loadusers(map<string, user> &users)
     file.close();
 }
 //----------------------------------------------
+//-------------------------------------------------------------
+
+
+void pass()
+{
+    int i=0;
+    char ch;
+
+    printf("\nThe password must be at least 8 characters"
+            "\nThe password may contain lowercase letters,uppercase letters, digits and special characters ");
+    printf("\nEnter the password: ");
+   while((ch=_getch())!=13)
+   {
+       regpass[i]=ch;
+       i++;
+       printf("*");
+   }
+    regpass[i]='\0';
+
+    int dig=0,low_alpha=0,upper_alpha=0,special=0;
+    for(int o=0;o<i;o++)
+    {
+        if(regpass[o]>='0'&&regpass[o]<='9')
+            dig+=1;
+    }
+    for(int o=0;o<i;o++)
+    {
+        if(regpass[o] >= 'a' && regpass[o] <= 'z' )
+            low_alpha+=1;
+    }
+    for(int o=0;o<i;o++)
+    {
+        if(regpass[o] >= 'A' && regpass[o] <= 'Z')
+            upper_alpha+=1;
+    }
+    for(int o=0;o<i;o++)
+    {
+        if(regpass[o]=='!'||regpass[o]=='@'||regpass[o]=='#'||regpass[o]=='$'||regpass[o]=='%'||regpass[o]=='^'||regpass[o]=='&'||regpass[o]=='*'||regpass[o]=='>'||regpass[o]=='<'||regpass[o]==','||regpass[o]=='.'||regpass[o]=='+'||regpass[o]=='='||regpass[o]=='-')
+            special+=1;
+    }
+    if(dig==0||low_alpha==0||upper_alpha==0||special==0||i<8)
+        {
+            cout<<"\nPassword is weak , enter another password!! ";
+             pass();
+        }
+    else
+    {
+      i=0;
+       char pwd2[100];
+       char e[100];
+       printf("\nRepeat the password: ");
+       while((ch=_getch())!=13)
+       {
+          pwd2[i]=ch;
+          i++;
+          printf("*");
+        }
+       pwd2[i]='\0';
+       if (strcmp(regpass,pwd2)==0)
+        {
+         for(int h=0;h<i;h++)
+            {
+              encryption(regpass[h]);
+              e[h]=y;
+            }
+          ofstream reg("users.txt",ios::app);
+          reg<<e<<endl;
+          reg.close();
+         }
+       else
+        {
+          printf("The passwords do not match");
+          pass();
+        }
+    }
+  }
+
+//------------------------------------------
